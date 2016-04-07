3d00aca3621aab4f3b6077309a859a4cf77acdf6
Mohammed El-Hakim <elhakim@gmail.com>
2016-04-07 12:14:20 +0200

    Update vim-installation.sh

--- vim-installation.sh
+++ vim-installation.sh
@@ -2,6 +2,6 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 wget "https://raw.githubusercontent.com/melhakim/configs/master/vimrc-clean" -O ~/.vimrc 
 vim +PluginInstall +qall
-cd ~/.vim/bundle/Improved-paragraph-motion/plugin && cat ipmotion.vim | sed s/\r//g > ipmotion &&\
+cd ~/.vim/bundle/Improved-paragraph-motion/plugin && cat ipmotion.vim | sed 's/\r$//' > ipmotion &&\
     mv ipmotion ipmotion.vim
 cd $OLDPWD
