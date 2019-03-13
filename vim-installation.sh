704091668d2a18c26438a6eaecbcac64e82424fa
mohammed-elhakim-sociomantic <mohammed.elhakim@sociomantic.com>
2019-03-13 12:31:00 +0100

    Update vim-installation.sh
    Fix ipmotion

--- vim-installation.sh
+++ vim-installation.sh
@@ -2,4 +2,4 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 wget "https://raw.githubusercontent.com/melhakim/configs/master/vimrc-clean" -O ~/.vimrc 
 vim +PluginInstall +qall
-sed -i 's/\r$//' ~/.vim/bundle/Improved-paragraph-motion/plugin/ipmotion.vim
+sed -i 's/\r//' ~/.vim/bundle/Improved-paragraph-motion/plugin/ipmotion.vim
